b0e3f6d7ccdf7ec144b3a8ab341d80af83e8c8b7
successdevop <successraphael28@gmail.com>
2023-04-03 22:58:08 +0100

    implemented the edit and delete funtionalities of the to-do app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // selecting elements
+const index = document.querySelector("#index");
 const addTask = document.getElementById("new-task-form");
 const taskInputValue = document.getElementById("newTask");
 const taskContainer = document.querySelector(".tasks");
@@ -12,7 +13,8 @@ taskContainer.innerHTML = "";
 savedTask = JSON.parse(localStorage.getItem("todoTask")) || [];
 
 // load on window refresh or reopening page
-savedTask.forEach(displayTodoUI);
+
+savedTask.forEach(displayTodo);
 
 // implementing functionalites
 addTask.addEventListener("submit", function (e) {
@@ -26,45 +28,8 @@ addTask.addEventListener("submit", function (e) {
     savedTask.push(taskValue);
     localStorage.setItem("todoTask", JSON.stringify(savedTask));
     taskInputValue.value = "";
-    displayTodoUI(taskValue);
+    displayTodo(taskValue);
   }
-});
-
-// implementing display UI functionality
-function displayTodoUI(taskValue) {
-  let htmlCode = "";
-  htmlCode += `
-      <div class="task">
-          <div class="task-content">
-            <input type="text" class="text" value="${taskValue}" readonly />
-          </div>
-          <div class="task-action-btns">
-            <button type="" class="edit">Edit</button>
-            <button type="" class="delete">Delete</button>
-          </div>
-      </div>`;
-
-  taskContainer.insertAdjacentHTML("beforeend", htmlCode);
-
-  // implementing task button functionalities
-}
-
-taskContainer.addEventListener("click", function (e) {
-  const element = e.target;
-  if (element.classList.contains("edit")) {
-    const myTarget =
-      element.parentElement.previousElementSibling.firstElementChild;
-
-    myTarget.removeAttribute("readonly");
-    myTarget.focus();
-    element.innerText = "save";
-  }
-  // else {
-  //   const myTarget =
-  //     element.parentElement.previousElementSibling.firstElementChild;
-  //   myTarget.setAttribute("readonly", "readonly");
-  //   element.innerText = "edit";
-  // }
 });
 
 deleteAllTask.addEventListener("click", function () {
@@ -76,102 +41,67 @@ deleteAllTask.addEventListener("click", function () {
   }
 });
 
-// function displayTodo() {
-//   let todoTask = localStorage.getItem("todoTask");
-//   if (todoTask === null) {
-//     todoArray = [];
-//   } else {
-//     todoArray = JSON.parse(todoTask);
-//   }
-//   // creating a dynamic element
-//   let htmlCode = "";
-//   todoArray.forEach((task) => {
-//     htmlCode += `
-//       <div class="task">
-//           <div class="task-content">
-//             <input type="text" class="text" value="${task}" readonly />
-//           </div>
-//           <div class="task-action-btns">
-//             <button type="" class="edit">Edit</button>
-//             <button type="" class="delete">Delete</button>
-//           </div>
-//       </div>`;
-//   });
-//   taskContainer.insertAdjacentHTML("beforeend", htmlCode);
+function displayTodo(text) {
+  //creating dynamic element
 
-//   //implementing button functionalities
-//   const btn = taskContainer.querySelectorAll(".task-action-btns");
-//   btn.forEach((eachBtnDiv) => {
-//     eachBtnDiv.addEventListener("click", function (e) {
-//       const target = e.target;
-//       if (target.classList.contains("delete")) {
-//         target.parentElement.parentElement.remove();
-//         // todoArray = todoArray.filter(e=>e!==)
-//       }
-//     });
-//   });
-// }
+  // creating the task element
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task");
+  // creating task input element div
+  const taskContentElement = document.createElement("div");
+  taskContentElement.classList.add("task-content");
+  // creating task input element
+  const task_input = document.createElement("input");
+  task_input.type = "text";
+  task_input.setAttribute("readonly", "readonly");
+  task_input.classList.add("text");
 
-// function displayTodo() {
-//   //creating dynamic element
+  task_input.value = text;
 
-//   // creating the task element
-//   const taskElement = document.createElement("div");
-//   taskElement.classList.add("task");
-//   // creating task input element div
-//   const taskContentElement = document.createElement("div");
-//   taskContentElement.classList.add("task-content");
-//   // creating task input element
-//   const task_input = document.createElement("input");
-//   task_input.type = "text";
-//   task_input.setAttribute("readonly", "readonly");
-//   task_input.classList.add("text");
+  // creating the task action buttons div
+  const childActionBtns = document.createElement("div");
+  childActionBtns.classList.add("task-action-btns");
+  // creating the task edit button
+  const btnEdit = document.createElement("button");
+  btnEdit.classList.add("edit");
+  btnEdit.innerText = "Edit";
+  // creating the task delete button
+  const btnDelete = document.createElement("button");
+  btnDelete.classList.add("delete");
+  btnDelete.innerText = "Delete";
+  // appending task buttons to task action buttons div
+  childActionBtns.appendChild(btnEdit);
+  childActionBtns.appendChild(btnDelete);
+  //appending task input to task input div
+  taskContentElement.appendChild(task_input);
+  // appending both task input element div and task action buttons div to task element
+  taskElement.appendChild(taskContentElement);
+  taskElement.appendChild(childActionBtns);
+  // appending task element to task container div
+  taskContainer.appendChild(taskElement);
 
-//   for (let i = 0; i < todoArray.length; i++) {
-//     task_input.value = todoArray[i];
-//   }
-//   // creating the task action buttons div
-//   const childActionBtns = document.createElement("div");
-//   childActionBtns.classList.add("task-action-btns");
-//   // creating the task edit button
-//   const btnEdit = document.createElement("button");
-//   btnEdit.classList.add("edit");
-//   btnEdit.innerText = "Edit";
-//   // creating the task delete button
-//   const btnDelete = document.createElement("button");
-//   btnDelete.classList.add("delete");
-//   btnDelete.innerText = "Delete";
-//   // appending task buttons to task action buttons div
-//   childActionBtns.appendChild(btnEdit);
-//   childActionBtns.appendChild(btnDelete);
-//   //appending task input to task input div
-//   taskContentElement.appendChild(task_input);
-//   // appending both task input element div and task action buttons div to task element
-//   taskElement.appendChild(taskContentElement);
-//   taskElement.appendChild(childActionBtns);
-//   // appending task element to task container div
-//   taskContainer.appendChild(taskElement);
+  // seting back our task input element to empty string
+  taskInputValue.value = "";
 
-//   // seting back our task input element to empty string
-//   taskInputValue.value = "";
+  // implementing functionality on the child buttons
+  childActionBtns.addEventListener("click", function (e) {
+    const target = e.target;
 
-//   // implementing functionality on the child buttons
-//   childActionBtns.addEventListener("click", function (e) {
-//     const target = e.target;
+    if (target.classList.contains("edit")) {
+      if (btnEdit.innerText.toLowerCase() === "edit") {
+        task_input.removeAttribute("readonly");
+        task_input.focus();
+        btnEdit.innerText = "save";
+      } else {
+        task_input.setAttribute("readonly", "readonly");
+        btnEdit.innerText = "edit";
+      }
+    }
 
-//     if (target.classList.contains("edit")) {
-//       if (btnEdit.innerText.toLowerCase() === "edit") {
-//         task_input.removeAttribute("readonly");
-//         task_input.focus();
-//         btnEdit.innerText = "save";
-//       } else {
-//         task_input.setAttribute("readonly", "readonly");
-//         btnEdit.innerText = "edit";
-//       }
-//     }
-
-//     if (target.classList.contains("delete")) {
-//       taskContainer.removeChild(taskElement);
-//     }
-//   });
-// }
+    if (target.classList.contains("delete")) {
+      taskContainer.removeChild(taskElement);
+      savedTask = savedTask.filter((val) => val !== text);
+      localStorage.setItem("todoTask", JSON.stringify(savedTask));
+    }
+  });
+}
